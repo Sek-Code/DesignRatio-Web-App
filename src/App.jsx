03cc309f957bb6e41093929a16d56f2b1e758b12
Jakkrit Saychang <56269911+boatlittlebear@users.noreply.github.com>
2025-12-18 10:52:00 +0700

    feat: implement account module with shared ui icons and components

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,12 @@
+import { Button } from "@/components/ui/button"
 function App() {
   return (
-    <h1 className="text-3xl font-bold underline text-red-500">
+    <>
+     <h1 className="text-3xl font-bold underline text-red-500">
       Hello Tailwind!
     </h1>
+      <Button>Click me</Button>
+    </>
   );
 }
 
---
+++ src/components/ui/icons/akar-icons-person.jsx
@@ -0,0 +1,27 @@
+import * as React from "react";
+
+export function PersonIcon({
+  size = 24,
+  color = "currentColor",
+  strokeWidth = 2,
+  className,
+  ...props
+}) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width={size}
+      height={size}
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke={color}
+      strokeWidth={strokeWidth}
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      className={className}
+      {...props}>
+      <circle cx="12" cy="7" r="5" /><path
+        d="M17 14h.352a3 3 0 0 1 2.976 2.628l.391 3.124A2 2 0 0 1 18.734 22H5.266a2 2 0 0 1-1.985-2.248l.39-3.124A3 3 0 0 1 6.649 14H7" />
+    </svg>
+  );
+}
