3b6bec8096efc76408a240a6c6cbcb7447baf11d
Jakkrit Saychang <56269911+boatlittlebear@users.noreply.github.com>
2025-12-19 00:19:16 +0700

    feat: routing + shadcn + components + logic + home render, brain running on low battery

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,8 @@
-import { Button } from "@/components/ui/button"
-function App() {
-  return (
-    <>
-     <h1 className="text-3xl font-bold underline text-red-500">
-      Hello Tailwind!
-      <br/>
-      i'm develop
-    </h1>
-      <Button>Click me</Button>
-    </>
-  );
-}
+import { RouterProvider } from 'react-router-dom';
+import { router } from './routes';
 
-export default App
+const App = () => {
+  return <RouterProvider router={router} />;
+};
+
+export default App;
