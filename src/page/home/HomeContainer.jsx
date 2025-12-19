f0fe31a8a0c55baa1b131eeb5a48eac4465f2775
Jakkrit Saychang <56269911+boatlittlebear@users.noreply.github.com>
2025-12-19 16:58:01 +0700

    feat(home): add home components and update HomeContainer structure

--- src/page/home/HomeContainer.jsx
+++ src/page/home/HomeContainer.jsx
@@ -1,38 +1,15 @@
-import { useEffect, useState } from "react";
+import CarouselImages from "./components/CarouselImage";
+import AboutUs from "./components/AboutUs";
+import CustomBlend from "./components/CustomBlend";
+import OurProducts from "./components/OurProducts";
 
 export default function HomeContainer() {
-  const [api, setApi] = useState(null);
-  const [current, setCurrent] = useState(1);
-  const [count, setCount] = useState(0);
-
-  const slides = [
-    { title: "Slide 1", desc: "HELLO SEKCODE #1" },
-    { title: "Slide 2", desc: "HELLO SEKCODE #2" },
-    { title: "Slide 3", desc: "HELLO SEKCODE #3" },
-  ];
-
-  useEffect(() => {
-    if (!api) return;
-
-    setCount(api.scrollSnapList().length);
-    setCurrent(api.selectedScrollSnap() + 1);
-
-    api.on("select", () => {
-      setCurrent(api.selectedScrollSnap() + 1);
-    });
-  }, [api]);
-
-  const handleNext = () => api?.scrollNext();
-  const handlePrev = () => api?.scrollPrev();
-  const handleGoTo = (index) => api?.scrollTo(index);
-
-  return {
-    slides,
-    current,
-    count,
-    setApi,
-    handleNext,
-    handlePrev,
-    handleGoTo,
-  };
+  return (
+    <>
+      <CarouselImages />
+      <AboutUs />
+      <CustomBlend />
+      <OurProducts />
+    </>
+  );
 }
