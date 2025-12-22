1f0ce0c2bc6ac9d2dcd8c321b21ad580013cab5b
ChutidetBen-oss <chutidet.benchathamloed@gmail.com>
2025-12-22 22:20:43 +0700

    start:Product page

--- src/page/Product-page/ProductPage.jsx
+++ src/page/Product-page/ProductPage.jsx
@@ -1,9 +1,9 @@
-import CardScreen from "@/components/ui/card/CardScreen"
+import CardScreen from "@/components/ui/card/CardScreen";
 
-export default function OurProduct() {
-    return (
+export default function ProductPage (){
+    return(
         <div className="justify-center items-center flex flex-col">
-            <h1 class="h1-style py-12 text-[#9e9957]">Our Products</h1>
+            <h1 class="h1-style py-12 text-[#9e9957]">Products</h1>
             <div className="flex flex-wrap justify-center mx-120">
                 <CardScreen></CardScreen>
                 <CardScreen></CardScreen>
@@ -12,10 +12,10 @@ export default function OurProduct() {
                 <CardScreen></CardScreen>
                 <CardScreen></CardScreen>
                 <CardScreen></CardScreen>
-                <CardScreen></CardScreen>
-                <CardScreen></CardScreen>
-                <CardScreen></CardScreen>
             </div>
+            <button className="button-style bg-[#411D03] text-[#f3ece3] w-35 h-8 rounded-2xl">
+                    <p>see more</p>
+            </button>
         </div>
     )
 }
--- src/page/home/HomeContainer.jsx
+++ src/page/home/HomeContainer.jsx
@@ -1,12 +1,12 @@
 
-
+import ProductPage from "../Product-page/ProductPage";
 import OurProducts from "./session/OurProducts";
 
 export default function HomeContainer() {
   return (
     <>
 
-      <OurProducts />
+      <ProductPage/>
     </>
   );
 }
