2510baeaa361cdb88c0c2006966a9b4794385ee1
Nattha <gamenattha555@gmail.com>
2025-12-24 21:37:57 +0700

    add button

--- src/page/home/session/OurProducts.jsx
+++ src/page/home/session/OurProducts.jsx
@@ -1,9 +1,12 @@
+import { Button } from "@/components/ui/button"
 import CardScreen from "@/components/ui/card/CardScreen"
+import { Link } from "react-router-dom"
+
 
 export default function OurProduct() {
     return (
         <div className="justify-center items-center flex flex-col">
-            <h1 class="h1-style py-12 text-[#9e9957]">Our Products</h1>
+            <h1 className ="h1-style py-12 text-[#9e9957]">Our Products</h1>
             <div className="flex flex-wrap justify-center mx-120">
                 <CardScreen></CardScreen>
                 <CardScreen></CardScreen>
@@ -16,6 +19,13 @@ export default function OurProduct() {
                 <CardScreen></CardScreen>
                 <CardScreen></CardScreen>
             </div>
+            <Link
+                to="/products"
+                className="hover:text-[(--matcha-color)] transition-colors">
+                <Button variant="default"  >
+                    See more
+                </Button>
+            </Link>
         </div>
     )
 }
