930c1d2c1d82dc4803a1f9dfd0363a08658e9a12
Jakkrit Saychang <56269911+boatlittlebear@users.noreply.github.com>
2025-12-27 01:35:40 +0700

    feat: add blending/contact routes and fix product navigation

--- src/constants/routes.constant.jsx
+++ src/constants/routes.constant.jsx
@@ -1,18 +1,18 @@
 import LayoutScreen from '../page/layout/LayoutScreen';
 import HomeScreen from '../page/home';
 import ProductPage from '../page/Product-page/ProductPage';
-import BlendingPage from '../page/blending/BlendingPage';
+import Blending from '../page/blending/Blending';
 import ContactPage from '../page/contact/ContactPage';
 
 const routes = [
   {
-    path: '',
+    path: '/',
     element: <LayoutScreen />,
     children: [
-      { path: '/', element: <HomeScreen /> },
-      { path: '/blending', element: <BlendingPage /> },
-      { path: '/product', element: <ProductPage /> },
-      { path: '/contact', element: <ContactPage /> },
+      { index: true, element: <HomeScreen /> },
+      { path: 'blending', element: <Blending /> },
+      { path: 'product', element: <ProductPage /> },
+      { path: 'contact', element: <ContactPage /> },
     ],
   },
 ];
