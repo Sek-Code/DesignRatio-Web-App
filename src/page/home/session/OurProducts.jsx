40a10719e800ede5cf9f47497b8be20d66ebdf47
Nattha <gamenattha555@gmail.com>
2025-12-29 00:24:23 +0700

    finish fixing card css

--- src/page/home/session/OurProducts.jsx
+++ src/page/home/session/OurProducts.jsx
@@ -6,13 +6,13 @@ import { Link } from "react-router-dom"
 
 export default function OurProduct() {
     const products = useProducts()
-    const productSection = products.slice(0, 10)
+    const productSection = products.slice(0, 6)
     //console.log(productSection)
     
     return (
-        <div className=" w-full">
+        <div className=" w-full px-[15%] flex-col items-center">
             <h1 className="h1-style py-12 text-[#9e9957] text-center">Our Products</h1>
-            <div className="w-full mx-[10%] mb-11 flex">
+            <div className="w-full  mb-11 flex">
                 <CardScreen  product={productSection}/>
                 
             </div>
