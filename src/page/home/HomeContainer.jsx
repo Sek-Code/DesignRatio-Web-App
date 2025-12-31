332a86353d14b4c8c1dd65b480b4b37bf6737770
Chalisa T. <chalisa.thanathi@gmail.com>
2026-01-01 00:05:33 +0700

    try to make radio button work

--- src/page/home/HomeContainer.jsx
+++ src/page/home/HomeContainer.jsx
@@ -2,7 +2,7 @@ import CarouselImages from "./session/CarouselImage";
 import AboutUs from "./session/AboutUs";
 import CustomBlend from "./session/CustomBlend";
 import OurProducts from "./session/OurProducts";
-import AddressBox from "../checkout/AddressBox";
+import DeliveryChoice from "../checkout/DeliveryChoice";
 
 export default function HomeContainer() {
   return (
@@ -11,7 +11,7 @@ export default function HomeContainer() {
       <AboutUs />
       <CustomBlend />
       <OurProducts />
-      <AddressBox />
+      <DeliveryChoice />
     </>
   );
 }
