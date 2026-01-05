429a0fe8a7fa1b7d891fb76a40c6b4d3fcb9e236
ChutidetBen-oss <chutidet.benchathamloed@gmail.com>
2026-01-05 23:03:57 +0700

    fix:size section hero in Checkout page

--- src/page/checkout/index.jsx
+++ src/page/checkout/index.jsx
@@ -1,6 +1,6 @@
 const Checkout = () => {
   return (
-    <div className="flex min-h-[50vh] items-center justify-center text-xl font-semibold text-brown">
+    <div className="flex min-h-[75vh] items-center justify-center text-xl font-semibold text-brown">
       Checkout page is under construction
     </div>
   );
