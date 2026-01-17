dd2eac1010db7a9cafc56e19c670264c185e6beb
Nattha <gamenattha555@gmail.com>
2026-01-17 22:06:41 +0700

    edit member in admin memberpage

--- src/page/userPage/contact/Banner.jsx
+++ src/page/userPage/contact/Banner.jsx
@@ -1,16 +1,17 @@
-
-
-export default function Banner(){
-    return (
+export default function Banner() {
+  return (
     <div className="relative w-full px-12 py-12  bg-[url('/img/banner.jpg')] bg-cover ">
       <div className="absolute bg-white/50  w-full inset-0"></div>
       <div className="relative z-10 flex flex-col items-center">
-      <h1 className=" h1-style text-brown mb-6">Contact US</h1>
-      <p className=" text-lg text-amber-900 w-[75%] text-center leading-relaxed drop-shadow-lg">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras sed lobortis sem. Quisque tempus tortor et sapien gravida aliquam. Quisque sit amet tincidunt sem. Cras fermentum tortor vitae interdum tempor. Duis mattis ullamcorper enim sed tempor. Quisque scelerisque sit amet tellus et placerat. Quisque sed suscipit neque. Quisque quis erat.
-      </p>
-     
+        <h1 className=" h1-style text-brown mb-6">Contact US</h1>
+        <p className=" text-lg text-amber-900 w-[75%] text-center leading-relaxed drop-shadow-lg">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras sed
+          lobortis sem. Quisque tempus tortor et sapien gravida aliquam. Quisque
+          sit amet tincidunt sem. Cras fermentum tortor vitae interdum tempor.
+          Duis mattis ullamcorper enim sed tempor. Quisque scelerisque sit amet
+          tellus et placerat. Quisque sed suscipit neque. Quisque quis erat.
+        </p>
       </div>
-        </div>
-    )
+    </div>
+  );
 }
--- src/page/userPage/contact/MemberCard.jsx
+++ src/page/userPage/contact/MemberCard.jsx
@@ -6,27 +6,26 @@ import {
   CardFooter,
   CardHeader,
   CardTitle,
-} from "@/components/ui/card"
+} from "@/components/ui/card";
 
-export default function MemberCard(member){
-    
-    const item = member.member
-    return (
-        <Card className="rounded-sm w-85 h-70 flex flex-col items-center">
-    
-        <div className="">
-        <img src={item.picture} className="w-26 h-26 rounded-full border  object-cover" />
-        </div>
-    <CardTitle>{item.name}</CardTitle>
-    
-   
-   
-    <CardContent>
-    <p>{item.text}</p>
-    </CardContent>
-    <CardFooter>
-    <p>{item.link}</p>
-    </CardFooter>
+export default function MemberCard(member) {
+  const item = member.member;
+  return (
+    <Card className="rounded-sm w-85 h-70 flex flex-col items-center">
+      <div className="">
+        <img
+          src={item.picture}
+          className="w-26 h-26 rounded-full border  object-cover"
+        />
+      </div>
+      <CardTitle>{item.name}</CardTitle>
+
+      <CardContent>
+        <p>{item.text}</p>
+      </CardContent>
+      <CardFooter>
+        <p>{item.link}</p>
+      </CardFooter>
     </Card>
-    )
+  );
 }
