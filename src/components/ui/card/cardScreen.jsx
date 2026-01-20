027f4d7b1bb4c26bb84f53e2b5f1563d26e83c15
ChutidetBen-oss <chutidet.benchathamloed@gmail.com>
2026-01-20 23:26:06 +0700

    fix: return onClick go to edit product page

--- src/components/ui/card/cardScreen.jsx
+++ src/components/ui/card/cardScreen.jsx
@@ -7,15 +7,17 @@ import { useState } from "react";
 function CardItem({ product, onCardClick}) {
   const { count, increment, decrement } = useCounter();
   const [selectedSize, setSelectedSize] = useState(null)
-  const sizeMap = Object.fromEntries(
-  (product.variants || []).map(v => [v.size, v])
-);
-
 
   return (
-    <div className="w-66 h-auto my-4 p-3 flex flex-col items-center gap-y-4">
+    <div
+  className="w-66 h-auto mb-15 p-3 flex flex-col items-center"
+  onClick={() => {
+    onCardClick?.(product);
+  }}
+>
 
-      <div>
+
+      <div className="">
         <img
           src={product.image}
           alt={product.name}
@@ -23,41 +25,41 @@ function CardItem({ product, onCardClick}) {
         />
       </div>
 
-      <h3 className="lg:text-2xl pt-5 pb-4 text-(--color-brown) truncate w-full text-center ">
+      <h3 className="h3-style pt-5 pb-4 text-[#411D03] truncate w-full text-center ">
         {product.name}
       </h3>
 
-      <SizeChoose
-        sizes={sizeMap}
-        selectedSize={selectedSize}
-        onSelect={setSelectedSize}
-      />
+    <SizeChoose
+      sizes={product.sizes || {}}
+      selectedSize={selectedSize}
+      onSelect={setSelectedSize}
+    />
 
-        <p className="lg:text-xl">
+        <p>
         Selected: {selectedSize || "—"}
         </p>
-        <p className="mb-3 lg:text-xl">
+        <p className="mb-3">
         Price: {selectedSize
-          ? sizeMap[selectedSize]?.price ?? "—"
-          : "—"} baht
-      </p>
+        ? product.sizes[selectedSize].price
+        : "—"} baht
+        </p>
 
-      <div className="flex flex-row items-center gap-5 h3-style text-(--color-brown) pb-5 ">
+      <div className="flex flex-row items-center gap-5 h3-style text-[#411D03] pb-5 ">
         <button
-          className="flex items-center justify-center border border-(--color-brown) rounded-full w-8 h-8 active:bg-(--color-brown) active:text-white"
+          className="flex items-center justify-center border border-[#411D03] rounded-full w-8 h-8 active:bg-[#411D03] active:text-white"
           onClick={decrement}
         >
           <p>-</p>
         </button>
         <p>{count}</p>
         <button
-          className="flex items-center justify-center border border-(--color-brown) rounded-full w-8 h-8 active:bg-(--color-brown) active:text-white"
+          className="flex items-center justify-center border border-[#411D03] rounded-full w-8 h-8 active:bg-[#411D03] active:text-white"
           onClick={increment}
         >
           <p>+</p>
         </button>
       </div>
-      <Button className="px-6 py-2 rounded-4xl lg:text-base lg:py-5 lg:px-8 lg:rounded-[30px] hover:bg-(--color-matcha)">
+      <Button variant="default" className=" bg-[#411D03] text-[#f3ece3] text-sm  ">
         Add to Cart
       </Button>
     </div>
@@ -66,7 +68,7 @@ function CardItem({ product, onCardClick}) {
 
 export default function CardScreen({onCardClick}) {
   return (
-    <div className="flex flex-wrap justify-center items-center w-full gap-4">
+    <div className="flex flex-wrap justify-center items-center w-full gap-7">
       {readyProducts.map((item) => (
         <CardItem key={item._id || item.name} product={item}
         onCardClick={onCardClick}/>
